2b2ff2573e97c80564f7202f4aab38a34f5c4e4f
toliadriapak <toliadriapak@gmail.com>
2022-10-29 22:58:36 +0300

    first changes

---
+++ day.js
@@ -0,0 +1,33 @@
+{   
+const input = document.querySelector("input")
+const button = document.querySelector("button")
+const handleClick = () => {
+  
+  const value = input.value.trim()
+  if (value) {
+    
+  alert(`Day: ${value}`)
+
+  }if (value > 7 || value < 0 || value == 0){
+  alert(`There is no such day. Try again`);
+  }if (value == 1){
+  alert('Monday');
+}if (value == 2){
+    alert('Tuesday');
+}if (value == 3){
+    alert('Wednesday');
+}if (value == 4){
+    alert('Thursday');
+}if (value == 5){
+    alert('Friday');
+}if (value == 6){
+    alert('Sunday');
+}if (value == 7){
+    alert('Saturday');
+  }  
+}
+
+button.addEventListener("click", handleClick)
+  
+}
+
